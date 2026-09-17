37b3de2f1bbe5ab0c72f05cd8c16d2a9dca6751e
Erika Monkevič <s066479@ad.viko.lt>
2026-09-17 11:16:34 +0300

    Atliktos tris pirmos uzduotis su tekstu

--- 1uzd.cpp
+++ 1uzd.cpp
@@ -1,3 +1,22 @@
-//
-// Created by s066479 on 9/17/2026.
-//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    /*string Vardas = "Erika", Pavarde = "Monkevic", Studijos = "Programu sistemos";
+    int Amzius = 20, Grupe = 2, Kursas = 1;
+    cout << Vardas << " " << Pavarde << " " << Amzius << " metu, " << Studijos << " " << Kursas << " kursas, " << Grupe << " grupe " << endl;*/
+
+   /* string Pavadinimas = "Banga", Savininkas = "Rimantas Mikalauskas", Stadionas = "Gargzduose";
+    int Metai = 1966, Vietos = 2300;
+    cout << "Futbolo klubas - " << Pavadinimas << " ikurtas " << Metai << " metais." << " Ju vadovas " << Savininkas << ". Stadionas randasi " << Stadionas << ", o ju talpa " << Vietos << " vietu." << endl;*/
+
+    /*string Marke = "TOYOTA", Modelis = "Avensis", Spalva = "sidabrines";
+    int Metai = 2009, Litrazas = 2;
+    cout << "Automobilis " << Marke << " " << Modelis << " yra pagamintas " << Metai << " metais. " << "Jos motoras " << Litrazas << " litrazo. " << "Automobilis yra " << Spalva << " spalvos." << endl; */
+
+    return 0;
+
+}
