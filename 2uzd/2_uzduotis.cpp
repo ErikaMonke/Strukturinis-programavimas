f6a1e5db1e164d41489528639f12ef86b4f1d62e
Erika Monkevič <s066479@ad.viko.lt>
2026-09-24 08:45:06 +0300

    Atlitka uzduotis su vidurkiu

--- 2uzd/2_uzduotis.cpp
+++ 2uzd/2_uzduotis.cpp
@@ -1,3 +1,13 @@
-//
-// Created by s066479 on 9/24/2026.
-//
+using namespace std;
+#include <iostream>
+
+int main() {
+    int v1, v2, v3, v4, v5;
+    double Vidurkis;
+    cout << "Iveskite vidurkius mokiniu atskirdami tarpais" << endl;
+    cin >> v1 >> v2 >> v3 >> v4 >> v5;
+    Vidurkis = (v1 + v2 + v3 + v4 + v5) / 5;
+    if (Vidurkis > 4) cout << "Vidurkis yra teigiamas: " << Vidurkis << endl;
+    else cout << "Vidurkis yra neigiamas: " << Vidurkis << endl;
+    return 0;
+}
