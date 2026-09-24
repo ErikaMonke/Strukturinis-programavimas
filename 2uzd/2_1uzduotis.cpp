c38b575d925a13005312879b7acbf873083153f0
Erika Monkevič <s066479@ad.viko.lt>
2026-09-24 10:22:16 +0300

    Pataisyti 2.1 uzduotyje kintamieji

--- 2uzd/2_1uzduotis.cpp
+++ 2uzd/2_1uzduotis.cpp
@@ -2,7 +2,7 @@ using namespace std;
 #include <iostream>
 
 int main() {
-    int v1, v2, v3, v4, v5;
+    double v1, v2, v3, v4, v5;
     double Vidurkis;
     cout << "Iveskite vidurkius mokiniu atskirdami tarpais" << endl;
     cin >> v1 >> v2 >> v3 >> v4 >> v5;
