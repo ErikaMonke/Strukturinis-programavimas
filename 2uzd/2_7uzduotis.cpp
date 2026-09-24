14426dc73646332ea1f9a7ef85c3ee3cbd7f36cb
Erika Monkevič <s066479@ad.viko.lt>
2026-09-24 09:43:04 +0300

    Istaisyta klaidas kur buvo parasyta skaiciu

--- 2uzd/2_7uzduotis.cpp
+++ 2uzd/2_7uzduotis.cpp
@@ -5,7 +5,7 @@ int main () {
     int s = 0;
     for (int i = 1; i <= 20; i++) {
         if (i % 4 == 0) {
-            cout << "Sis skaiciu dalinasi is 4: " << i << endl;
+            cout << "Sis skaicius dalinasi is 4: " << i << endl;
         }
     }
     return 0;
