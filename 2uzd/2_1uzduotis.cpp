b5609f3b53c78020cd3dfc8c6eb3c72366146d00
Erika Monkevič <s066479@ad.viko.lt>
2026-09-24 10:24:55 +0300

    Pataisyta 2.3 uzduotis

--- 2uzd/2_1uzduotis.cpp
+++ 2uzd/2_1uzduotis.cpp
@@ -7,7 +7,7 @@ int main() {
     cout << "Iveskite vidurkius mokiniu atskirdami tarpais" << endl;
     cin >> v1 >> v2 >> v3 >> v4 >> v5;
     Vidurkis = (v1 + v2 + v3 + v4 + v5) / 5;
-    if (Vidurkis > 4) cout << "Vidurkis yra teigiamas: " << Vidurkis << endl;
+    if (Vidurkis >= 5) cout << "Vidurkis yra teigiamas: " << Vidurkis << endl;
     else cout << "Vidurkis yra neigiamas: " << Vidurkis << endl;
     return 0;
 }
