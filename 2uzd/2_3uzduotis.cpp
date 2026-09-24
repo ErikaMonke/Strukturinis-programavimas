df65a19b02898fd8ee839a11de7cecc2564b842d
Erika Monkevič <s066479@ad.viko.lt>
2026-09-24 09:04:57 +0300

    Atlikta 2.3 uzduotis su egzaminu

--- 2uzd/2_3uzduotis.cpp
+++ 2uzd/2_3uzduotis.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main () {
-    
-    return;
+    double egz;
+    cout << "Iveskite egzamino ivertinima: " << endl;
+    cin >> egz;
+    if (egz == 10) cout << "Puiku" << endl;
+    else if (egz >= 9 && egz < 10) cout << "Labai gerai" << endl;
+    else if (egz >= 7 && egz < 9) cout << "Gerai" << endl;
+    else if (egz >= 5 && egz < 7) cout << "Patenkinamai" << endl;
+    else if (egz < 5 && egz > 0) cout << "Egzaminas neislaikytas" << endl;
+    else cout << "Blogai ivestas rezultatas. Paleiskite programa is naujo" << endl;
+    return 0;
 }
