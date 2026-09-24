b70e0d4bc586f074f4f22242b5d15d9abf22bb1f
Erika Monkevič <s066479@ad.viko.lt>
2026-09-24 09:20:37 +0300

    Atlikta 2.4 uzduotis

--- 2uzd/2_4uzduotis.cpp
+++ 2uzd/2_4uzduotis.cpp
@@ -1,3 +1,44 @@
-//
-// Created by s066479 on 9/24/2026.
-//
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int egz;
+    cout << "Iveskite egzamino ivertinima: " << endl;
+    cin >> egz;
+    switch (egz) {
+        case 10:
+            cout << "Puiku" << endl;
+            break;
+        case 9:
+            cout << "Labai gerai" << endl;
+            break;
+        case 8:
+            cout << "Gerai" << endl;
+            break;
+        case 7:
+            cout << "Gerai" << endl;
+            break;
+        case 6:
+            cout << "Patenkinamai" << endl;
+            break;
+        case 5:
+            cout << "Patenkinamai" << endl;
+            break;
+        case 4:
+            cout << "Egzaminas neislaikytas" << endl;
+            break;
+        case 3:
+            cout << "Egzaminas neislaikytas" << endl;
+            break;
+        case 2:
+            cout << "Egzaminas neislaikytas" << endl;
+            break;
+        case 1:
+            cout << "Egzaminas neislaikytas" << endl;
+            break;
+        default:
+            cout << "Blogai ivestas rezultatas. Paleiskite programa is naujo" << endl;
+    }
+    return 0;
+}
