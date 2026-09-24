a08170966a1a7293d066d6e54a73c2f224011362
Erika Monkevič <s066479@ad.viko.lt>
2026-09-24 09:29:38 +0300

    Atlikta 2.5 uzduotis

--- 2uzd/2_5uzduotis.cpp
+++ 2uzd/2_5uzduotis.cpp
@@ -1,3 +1,15 @@
-//
-// Created by s066479 on 9/24/2026.
-//
+#include <iostream>
+
+using namespace std;
+int main () {
+    int Suma = 0;
+    for (int i = 1; i <= 20; i++) {
+        if (i % 2 != 0) {
+            Suma += i;
+            cout << "Skaicius: " << i << endl;
+        }
+
+    }
+    cout << "Suma visu nelyginiu skaiciu nuo 1 iki 20: " << Suma << endl;
+    return 0;
+}
